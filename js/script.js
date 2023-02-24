f0e82522a8a211c84fd8fe18b0bb8f86f84087db
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-24 21:53:58 +0300

    form login

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ searchBtn.addEventListener('click', () => {
     searchBar.classList.toggle('active');
 });
 // ##############################################
+
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+});
