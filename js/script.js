2fc45dbbc6926d3701b386fd645448134cf99d3c
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-25 17:28:30 +0300

    home section

--- js/script.js
+++ js/script.js
@@ -35,4 +35,22 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
+// ##############################################
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+
+});
+
+
+
+
+
+
 
