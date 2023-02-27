336dd8698867994f5fd7892cabb5c12988dba058
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-28 00:14:38 +0300

    review section

--- js/script.js
+++ js/script.js
@@ -49,6 +49,27 @@ videoBtn.forEach(btn => {
 });
 
 
+var swiper = new Swiper(".review-silder", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    },
+});
+
+
 
 
 
